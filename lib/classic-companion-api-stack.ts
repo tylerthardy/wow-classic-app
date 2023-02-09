224af419fd57227a3961bd7eabe2d79927d6a7a4
Tyler Hardy <tylerthardy@gmail.com>
2023-02-09 12:18:24 -0500

    Clean up stack and resource names

--- lib/classic-companion-api-stack.ts
+++ lib/classic-companion-api-stack.ts
@@ -4,11 +4,11 @@ import { Code, Function, LayerVersion, Runtime } from "aws-cdk-lib/aws-lambda";
 import { Construct } from "constructs";
 import path = require("path");
 
-export class CdkStack extends Stack {
+export class ClassicCompanionApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lambdaLayer = new LayerVersion(this, "HandlerLayer", {
+    const lambdaLayer = new LayerVersion(this, "node-module-layer", {
       code: Code.fromAsset(
         path.resolve(__dirname, "../api/node-modules-layer")
       ),
@@ -17,7 +17,7 @@ export class CdkStack extends Stack {
     });
 
     // add handler to respond to all our api requests
-    const handler = new Function(this, "Handler", {
+    const handler = new Function(this, "nestjs-handler", {
       code: Code.fromAsset(path.resolve(__dirname, "../api/dist")),
       handler: "lambda.handler",
       runtime: Runtime.NODEJS_16_X,
@@ -25,7 +25,7 @@ export class CdkStack extends Stack {
       timeout: Duration.seconds(30),
     });
 
-    new LambdaRestApi(this, "HandlerApiGateway", {
+    new LambdaRestApi(this, "nestjs-api-gateway", {
       handler: handler,
     });
   }
