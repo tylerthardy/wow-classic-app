b37254f98280d9425bad711d2fbbd894e1530a74
Tyler Hardy <tylerthardy@gmail.com>
2023-02-10 12:53:08 -0500

    Add lambda insights

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -1,32 +1,46 @@
-import { Duration, Stack, StackProps } from "aws-cdk-lib";
-import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
-import { Code, Function, LayerVersion, Runtime } from "aws-cdk-lib/aws-lambda";
-import { Construct } from "constructs";
-import path = require("path");
+import { Duration, Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
+import { Code, Function, LayerVersion, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import path = require('path');
 
 export class ClassicCompanionApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const lambdaLayer = new LayerVersion(this, "node-module-layer", {
-      code: Code.fromAsset(
-        path.resolve(__dirname, "../api/node-modules-layer")
-      ),
+    const lambdaLayer = new LayerVersion(this, 'node-module-layer', {
+      code: Code.fromAsset(path.resolve(__dirname, '../api/node-modules-layer')),
       compatibleRuntimes: [Runtime.NODEJS_16_X],
-      description: "Api Handler Dependencies",
+      description: 'Api Handler Dependencies'
     });
+
+    const insightsLayer = LayerVersion.fromLayerVersionArn(this, `lambda-insights-layer`, this.getLambdaInsightsArn());
 
     // add handler to respond to all our api requests
-    const handler = new Function(this, "nestjs-handler", {
-      code: Code.fromAsset(path.resolve(__dirname, "../api/dist")),
-      handler: "lambda.handler",
+    const handler = new Function(this, 'nestjs-handler', {
+      code: Code.fromAsset(path.resolve(__dirname, '../api/dist')),
+      handler: 'lambda.handler',
       runtime: Runtime.NODEJS_16_X,
-      layers: [lambdaLayer],
+      layers: [lambdaLayer, insightsLayer],
       timeout: Duration.seconds(30),
+      memorySize: 512
     });
 
-    new LambdaRestApi(this, "nestjs-api-gateway", {
-      handler: handler,
+    if (!handler.role) {
+      throw new Error('no implicit role for handler lambda');
+    }
+    handler.role!.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName('CloudWatchLambdaInsightsExecutionRolePolicy')
+    );
+
+    new LambdaRestApi(this, 'nestjs-api-gateway', {
+      handler: handler
     });
+  }
+
+  private getLambdaInsightsArn(region: string = 'us-east-1', version: number = 21): string {
+    const insightsLayerArn = `arn:aws:lambda:${region}:580247275435:layer:LambdaInsightsExtension:${version}`;
+    return insightsLayerArn;
   }
 }
