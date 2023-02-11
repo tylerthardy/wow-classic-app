68ff8056497f71b98b7bdf75b116169901415309
Tyler Hardy <tylerthardy@gmail.com>
2023-02-11 12:53:55 -0500

    Fix multiple character validation

--- api/src/warcraft-logs/character/requests/get-multiple-character-zone-rankings-request.ts
+++ api/src/warcraft-logs/character/requests/get-multiple-character-zone-rankings-request.ts
@@ -1,12 +1,12 @@
 import { Type } from 'class-transformer';
-import { IsArray, MaxLength, MinLength, ValidateNested } from 'class-validator';
+import { ArrayMaxSize, ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
 import { GetCharacterZoneRankingsRequest } from './get-character-zone-rankings-request';
 import { IGetMultipleCharacterZoneRankingsRequest } from './get-multiple-character-zone-rankings-request.interface';
 
 export class GetMultipleCharacterZoneRankingsRequest implements IGetMultipleCharacterZoneRankingsRequest {
   @IsArray()
-  @MinLength(1)
-  @MaxLength(40)
+  @ArrayMinSize(1)
+  @ArrayMaxSize(40)
   @ValidateNested({ each: true })
   @Type(() => GetCharacterZoneRankingsRequest)
   characters: GetCharacterZoneRankingsRequest[];
