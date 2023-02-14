7288845fbea83608817e4ddd866147883249be07
Tyler Hardy <tylerthardy@gmail.com>
2023-02-14 10:46:51 -0500

    Remove limit on raid lookup array

--- api/src/character/requests/get-multiple-character-zone-rankings-request.ts
+++ api/src/character/requests/get-multiple-character-zone-rankings-request.ts
@@ -1,12 +1,11 @@
 import { Type } from 'class-transformer';
-import { ArrayMaxSize, ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
+import { ArrayMinSize, IsArray, ValidateNested } from 'class-validator';
 import { GetCharacterZoneRankingsRequest } from './get-character-zone-rankings-request';
 import { IGetMultipleCharacterZoneRankingsRequest } from './get-multiple-character-zone-rankings-request.interface';
 
 export class GetMultipleCharacterZoneRankingsRequest implements IGetMultipleCharacterZoneRankingsRequest {
   @IsArray()
   @ArrayMinSize(1)
-  @ArrayMaxSize(40)
   @ValidateNested({ each: true })
   @Type(() => GetCharacterZoneRankingsRequest)
   characters: GetCharacterZoneRankingsRequest[];
