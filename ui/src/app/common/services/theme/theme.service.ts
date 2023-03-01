06917502f975ed4599d897a92166c8de7e4f819f
Tyler Hardy <tylerthardy@gmail.com>
2023-03-01 15:10:32 -0500

    Persist theme

--- ui/src/app/common/services/theme/theme.service.ts
+++ ui/src/app/common/services/theme/theme.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { LocalStorageService } from '../local-storage.service';
 import { Theme } from './theme.type';
 
 @Injectable({
@@ -6,7 +7,12 @@ import { Theme } from './theme.type';
 })
 export class ThemeService {
   public theme: Theme = 'dark';
-  constructor() {}
+  constructor(private localStorageService: LocalStorageService) {
+    const theme: Theme = this.localStorageService.get('theme', 'userTheme');
+    if (theme) {
+      this.theme = theme;
+    }
+  }
 
   public getThemeClass(): { [key: string]: any } {
     const theme: { [key: string]: any } = this.theme === 'light' ? { 'light-theme': true } : { 'dark-theme': true };
@@ -15,5 +21,10 @@ export class ThemeService {
 
   public toggleTheme(): void {
     this.theme = this.theme === 'light' ? 'dark' : 'light';
+    this.storeTheme();
+  }
+
+  private storeTheme(): void {
+    this.localStorageService.store('theme', 'userTheme', this.theme);
   }
 }
