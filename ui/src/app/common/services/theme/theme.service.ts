e2c8a0a0e5c367dd071e2fb629b0dfa7375bcb06
Tyler Hardy <tylerthardy@gmail.com>
2023-03-20 03:10:09 -0400

    html based dark theme

--- ui/src/app/common/services/theme/theme.service.ts
+++ ui/src/app/common/services/theme/theme.service.ts
@@ -11,6 +11,7 @@ export class ThemeService {
     const theme: Theme = this.localStorageService.get('theme', 'userTheme');
     if (theme) {
       this.theme = theme;
+      this.setHtmlTheme();
     }
   }
 
@@ -21,7 +22,19 @@ export class ThemeService {
 
   public toggleTheme(): void {
     this.theme = this.theme === 'light' ? 'dark' : 'light';
+    this.setHtmlTheme();
     this.storeTheme();
+  }
+
+  public setHtmlTheme(): void {
+    // remove scrollbars
+    document.documentElement.style.overflow = 'hidden';
+    // trigger reflow so that overflow style is applied
+    document.body.clientWidth;
+    // change scheme
+    document.documentElement.setAttribute('data-color-scheme', this.theme === 'light' ? 'light' : 'dark');
+    // remove overflow style, which will bring back the scrollbar with the correct scheme
+    document.documentElement.style.overflow = '';
   }
 
   private storeTheme(): void {
