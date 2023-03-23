fd53df178763ea40d96e2b1d99c6ca1d9fee04d8
Tyler Hardy <tylerthardy@gmail.com>
2023-03-23 15:56:41 -0400

    Maintenance off

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://6zdrton9kb.execute-api.us-east-1.amazonaws.com/prod',
   discordUrl: 'https://discord.gg/dJ8YvKn8AR',
   addonDownloadUrl: 'https://wowclassicapp-addon.s3.amazonaws.com/GroupMembersExporter.zip',
-  maintenance: true
+  maintenance: false
 };
