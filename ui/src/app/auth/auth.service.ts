f877f7fe1d7b12a518f1e77a98c2bede67bfd831
Tyler Hardy <tylerthardy@gmail.com>
2023-03-24 14:07:56 -0400

    Auth improvements

--- ui/src/app/auth/auth.service.ts
+++ ui/src/app/auth/auth.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
-import { CognitoUserPool, CognitoUserSession, ICognitoUserPoolData } from 'amazon-cognito-identity-js';
+import {
+  AuthenticationDetails,
+  CognitoUser,
+  CognitoUserPool,
+  CognitoUserSession,
+  IAuthenticationCallback,
+  ICognitoUserPoolData
+} from 'amazon-cognito-identity-js';
 import { AppConfig } from '../config/app.config';
+import { ApplicationUser } from './application-user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  public user: ApplicationUser | undefined;
   private userpool: CognitoUserPool;
 
   constructor(private appConfig: AppConfig) {
@@ -17,10 +26,48 @@ export class AuthService {
     this.userpool = new CognitoUserPool(poolData);
   }
 
+  public signIn(emailAddress: string, password: string, resultCallbacks?: IAuthenticationCallback): void {
+    let authenticationDetails = new AuthenticationDetails({
+      Username: emailAddress,
+      Password: password
+    });
+
+    let userData = { Username: emailAddress, Pool: this.userpool };
+    var cognitoUser = new CognitoUser(userData);
+    let authCallbacks: IAuthenticationCallback = {
+      onSuccess: (result: CognitoUserSession) => {
+        this.setUser(result);
+        resultCallbacks?.onSuccess(result);
+      },
+      onFailure: (err) => {
+        alert(err.message || JSON.stringify(err));
+        resultCallbacks?.onFailure(err);
+      },
+      newPasswordRequired: (userAttributes, requiredAttributes) => {
+        // User was signed up by an admin and must provide new
+        // password and required attributes, if any, to complete
+        // authentication.
+
+        // the api doesn't accept this field back
+        delete userAttributes.email_verified;
+
+        // store userAttributes on global variable
+        cognitoUser.completeNewPasswordChallenge('Potato123', userAttributes, authCallbacks);
+
+        if (resultCallbacks?.newPasswordRequired) {
+          resultCallbacks.newPasswordRequired(userAttributes, requiredAttributes);
+        }
+      }
+    };
+    cognitoUser.authenticateUser(authenticationDetails, authCallbacks);
+  }
+
   public signOut(): void {
     const cognitoUser = this.userpool.getCurrentUser();
     if (cognitoUser) {
-      cognitoUser.signOut();
+      cognitoUser.signOut(() => {
+        this.setUser(undefined);
+      });
     }
   }
 
@@ -31,6 +78,7 @@ export class AuthService {
         return new Promise((resolve, reject) => {
           cognitoUser.getSession((err: any, session: CognitoUserSession) => {
             if (err) return reject(err);
+            this.setUser(session);
             resolve(session.getIdToken().getJwtToken());
           });
         });
@@ -39,7 +87,8 @@ export class AuthService {
     return Promise.resolve(undefined);
   }
 
-  // FIXME: This should be set by the api middleware, not calculated in the property. The property calculation can cause the UI to spam cognito with requests
+  // FIXME: This should be set by the api middleware, not calculated in the property.
+  // The property calculation can cause the UI to spam cognito with requests because getSession will refresh a token
   get isLoggedIn(): boolean {
     let isAuth = false;
 
@@ -52,11 +101,19 @@ export class AuthService {
         } else if (!session) {
           alert(err.message || JSON.stringify(err));
         } else {
+          this.setUser(session);
           isAuth = session.isValid();
         }
       });
     }
-    console.log('evaluating isLoggedIn', isAuth);
     return isAuth;
+  }
+
+  private setUser(session: CognitoUserSession | undefined) {
+    if (!session) {
+      this.user = undefined;
+      return;
+    }
+    this.user = new ApplicationUser(session);
   }
 }
--- ui/src/app/auth/sign-in-modal/sign-in-modal.component.ts
+++ ui/src/app/auth/sign-in-modal/sign-in-modal.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import {
-  AuthenticationDetails,
-  CognitoUser,
-  CognitoUserPool,
-  CognitoUserSession,
-  IAuthenticationCallback
-} from 'amazon-cognito-identity-js';
+import { CognitoUserSession } from 'amazon-cognito-identity-js';
 import { SimpleModalComponent } from 'ngx-simple-modal';
-import { environment } from '../../../environments/environment';
 import { ThemeService } from '../../common/services/theme/theme.service';
+import { AuthService } from '../auth.service';
 
 @Component({
   templateUrl: 'sign-in-modal.component.html'
@@ -19,47 +13,23 @@ export class SignInModalComponent extends SimpleModalComponent<any, CognitoUserS
   email_address: string = '';
   password: string = '';
 
-  constructor(public themeService: ThemeService) {
+  constructor(public authService: AuthService, public themeService: ThemeService) {
     super();
   }
 
   onSignIn(form: NgForm) {
     if (form.valid) {
       this.isLoading = true;
-      let authenticationDetails = new AuthenticationDetails({
-        Username: this.email_address,
-        Password: this.password
-      });
-      let poolData = {
-        UserPoolId: environment.cognito.userPoolId,
-        ClientId: environment.cognito.userPoolWebClientId
-      };
-
-      let userPool = new CognitoUserPool(poolData);
-      let userData = { Username: this.email_address, Pool: userPool };
-      var cognitoUser = new CognitoUser(userData);
-      let authCallbacks: IAuthenticationCallback = {
+      this.authService.signIn(this.email_address, this.password, {
         onSuccess: (result) => {
-          this.result = result;
           this.close();
         },
         onFailure: (err) => {
           alert(err.message || JSON.stringify(err));
           this.isLoading = false;
         },
-        newPasswordRequired: (userAttributes, requiredAttributes) => {
-          // User was signed up by an admin and must provide new
-          // password and required attributes, if any, to complete
-          // authentication.
-
-          // the api doesn't accept this field back
-          delete userAttributes.email_verified;
-
-          // store userAttributes on global variable
-          cognitoUser.completeNewPasswordChallenge('Potato123', userAttributes, authCallbacks);
-        }
-      };
-      cognitoUser.authenticateUser(authenticationDetails, authCallbacks);
+        newPasswordRequired: (userAttributes, requiredAttributes) => {}
+      });
     }
   }
 }
