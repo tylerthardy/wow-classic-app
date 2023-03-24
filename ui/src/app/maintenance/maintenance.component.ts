41b7c1a82bf30cd53514f6621b6d501033d258f3
Tyler Hardy <tylerthardy@gmail.com>
2023-03-24 13:30:10 -0400

    konami bypass maintenance

--- ui/src/app/maintenance/maintenance.component.ts
+++ ui/src/app/maintenance/maintenance.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LocalStorageService } from '../common/services/local-storage.service';
 import { AppConfig } from '../config/app.config';
 
 @Component({
@@ -8,7 +9,7 @@ import { AppConfig } from '../config/app.config';
   styleUrls: ['./maintenance.component.scss']
 })
 export class MaintenanceComponent implements OnInit {
-  constructor(private appConfig: AppConfig, private router: Router) {}
+  constructor(private appConfig: AppConfig, private localStorageService: LocalStorageService, private router: Router) {}
 
   ngOnInit(): void {
     if (!this.appConfig.maintenance) {
@@ -18,5 +19,9 @@ export class MaintenanceComponent implements OnInit {
 
   public onDiscordIconClick() {
     window.open(this.appConfig.discordUrl, '_blank');
+  }
+
+  public onKonami() {
+    this.localStorageService.store('maintenance', 'bypass', true);
   }
 }
