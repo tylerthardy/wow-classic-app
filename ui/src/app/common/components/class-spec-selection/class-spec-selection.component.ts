332c159c7dd72bfbec61e84057a5f7d2cc5ca926
Tyler Hardy <tylerthardy@gmail.com>
2023-08-25 22:45:06 -0400

    Restore wcl meme specs

--- ui/src/app/common/components/class-spec-selection/class-spec-selection.component.ts
+++ ui/src/app/common/components/class-spec-selection/class-spec-selection.component.ts
@@ -23,7 +23,7 @@ export class ClassSpecSelectionComponent implements ControlValueAccessor, OnInit
   @Input() removeBlank: boolean = false;
   @Input() iconOnly: boolean = false;
   public selectedSpec: SpecializationData | undefined;
-  public specializations: SpecializationData[] = specializations.filter((s) => !s.isWarcraftLogsOnly);
+  public specializations: SpecializationData[] = specializations;
   public isDropdownShown: boolean = false;
   public filteredSpecs: SpecializationData[] = [];
 
@@ -79,10 +79,7 @@ export class ClassSpecSelectionComponent implements ControlValueAccessor, OnInit
 
   // FIXME: This should be part of a data structure instead of having to loop through and map weird shit
   private setSpecsToClass(classId: number | undefined): void {
-    this.filteredSpecs = Object.assign(
-      [],
-      specializations.filter((s) => !s.isWarcraftLogsOnly)
-    );
+    this.filteredSpecs = Object.assign([], specializations);
     if (classId) {
       const wowClass: WowClass | undefined = WowClasses.getClassByWarcraftLogsId(classId);
       if (!wowClass) {
