68f808ced34cd1c5e8e01b45cabe4a3867040932
Tyler Hardy <tylerthardy@gmail.com>
2023-10-06 03:26:15 -0400

    Initial ICC support

--- api/src/warcraft-logs/warcraft-logs.service.ts
+++ api/src/warcraft-logs/warcraft-logs.service.ts
@@ -120,10 +120,18 @@ export class WarcraftLogsService {
           }
         `;
 
-    const result: ApolloQueryResult<CharacterData> = await this.apollo.query({
-      query: GET_CHARACTER_ZONE_RANKINGS,
-      fetchPolicy: 'network-only'
-    });
+    let result: ApolloQueryResult<CharacterData>;
+    console.log('querying wcl for character');
+    try {
+      result = await this.apollo.query({
+        query: GET_CHARACTER_ZONE_RANKINGS,
+        fetchPolicy: 'network-only'
+      });
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+    console.log('query complete');
 
     // FIXME: Hacky. There should be more defined types
     return {
