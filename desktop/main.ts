63c92cb81487757c70588bac2ffdd998083ab716
Tyler Hardy <tylerthardy@gmail.com>
2023-10-23 14:21:10 -0400

    Improve electron devx

--- desktop/main.ts
+++ desktop/main.ts
@@ -22,7 +22,7 @@ function createWindow() {
   // load the dist folder from Angular
   win.loadURL(
     url.format({
-      pathname: path.join(__dirname, '/dist/index.html'),
+      pathname: path.join(__dirname, '../dist/index.html'),
       protocol: 'file:',
       slashes: true
     })
